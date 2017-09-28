cb9995269e48a5feec167dd2efeb6f72b223642e
Jodi Diehl <29265610+jdiehl99@users.noreply.github.com>
2017-09-28 12:43:25 -0400

    finished cloze cards

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Flashcard-Generator
 
+## Basic Flashcards
 ![Flashcard Generator](basicdemo.gif "Flashcard Generator")
+
+## Cloze Flashcards
+![Flashcard Generator](clozedemo.gif "Flashcard Generator")
--- closeCard.js
+++ closeCard.js
@@ -1,23 +1,50 @@
 //requires
 
-var basicCard = require('./library/close.js');
-var cardDate = require('./cloze.json');
+var basicCard = require('./library/cloze.js');
+var cardData = require('./cloze.json');
 var inquirer = require('inquirer');
 
-startgame();
+var count = 0;
+var answerRight = 0;
+var answerWrong = 0;
 
-function startGame(){
-    //code
+arrlength = cardData.length;
+showQuestion(arrlength,count);
+
+function showQuestion(arrlength,count) {
+
+    // replaces all instance of Microsoft with W3Schools
+    // var res = str.replace("Microsoft", "W3Schools");
+
+    var str = cardData[count].front; 
+    var res = str.replace(cardData[count].back, "____________");
+
+    // show user the next question
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "userGuess",
+            message: res
+        }
+    ]).then(function (guess) {
+        if (guess.userGuess === cardData[count].back) {
+            console.log("You are correct");
+            answerRight++;
+        } else {
+            console.log("You are wrong - the correct answer is " + cardData[count].back);
+            answerWrong++;
+        }
+        count++;
+        checkCount(arrlength,count);
+    });
 }
 
-function endGame(){
-    //codes
-}
 
-function round(){
-    //codes
-}
 
-function prompts(){
-    //codes
+function checkCount(arrlength,count) {
+    if (count < 2) {
+        showQuestion(arrlength,count);
+    } else { // game over
+        console.log("You answered " + answerRight + " questions correctly and missed " + answerWrong + " questions");
+    }
 }
