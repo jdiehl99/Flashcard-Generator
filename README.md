26beda0b647da70f05e2353b5a9be4c66ec15cd8
Jodi Diehl <29265610+jdiehl99@users.noreply.github.com>
2017-09-28 09:22:39 -0400

    added gif for read me - basic cards are complete

--- README.md
+++ README.md
@@ -1,28 +1,3 @@
 # Flashcard-Generator
 
-library / basic.js 
-library / cloze.js 
-node_modules (folder)
-basic.json
-basicCard.js
-closeCard.js
-cloze.json
-package.json
-
-// cloze - compare answer to question (what's in the partial)
-2 constructors -
-basicCard(front,back)
-clozeCard(something,something) -- 2 functions both for logic and probably a constructor
-
-var basicCard = require ./library/basic.js
-var cardData = require ./cloze.json
-var inquirer = require inquirer
-
-startGame()
-
-function startGame()
-function endGame()
-function round()
-function prompts()
-
-questions in a file - go through constructor, then display to user
+![Flashcard Generator](basicdemo.gif "Flashcard Generator")
--- basicCard.js
+++ basicCard.js
@@ -4,23 +4,39 @@ var basicCard = require('./library/basic.js');
 var cardData = require('./basic.json');
 var inquirer = require('inquirer');
 
-console.log(cardData[1].front);
-// startgame();
+var count = 0;
+var answerRight = 0;
+var answerWrong = 0;
 
-function startGame(){
-    //code
+arrlength = cardData.length;
+showQuestion(arrlength,count);
+
+function showQuestion(arrlength,count) {
+    // show user the next question
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "userGuess",
+            message: cardData[count].front
+        }
+    ]).then(function (guess) {
+        if (guess.userGuess === cardData[count].back) {
+            console.log("You are correct");
+            answerRight++;
+        } else {
+            console.log("You are wrong - the correct answer is " + cardData[count].back);
+            answerWrong++;
+        }
+        count++;
+        checkCount(arrlength,count);
+    });
 }
 
-function endGame(){
-    //codes
-}
 
-function round(){
-    //codes
+function checkCount(arrlength,count) {
+    if (count < 2) {
+        showQuestion(arrlength,count);
+    } else { // game over
+        console.log("You answered " + answerRight + " questions correctly and missed " + answerWrong + " questions");
+    }
 }
-
-function prompts(){
-    //codes
-}
-
-// console.log(object);
